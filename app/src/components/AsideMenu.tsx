79033a296614544ca2e71c29dde125cfa2e09438
valdeci <moura97.valdeci@gmail.com>
2023-04-28 14:09:13 -0300

    feat(components): create aside menu

---
+++ app/src/components/AsideMenu.tsx
@@ -0,0 +1,37 @@
+import SVGIcon from './SVGIcon';
+import { linkOptions } from '../utils/constants/link';
+import { icons } from '../utils/icons';
+import '../styles/aside-menu.css';
+
+type AsideMenuProps = {
+  menuProps: { menuClass: string };
+};
+
+export default function AsideMenu({ menuProps }: AsideMenuProps) {
+  return (
+    <aside className={`aside-menu-container ${menuProps.menuClass}`}>
+      <nav className="aside-menu-link-container">
+        {linkOptions.map((link) => (
+          <a href={link.href} key={link.href} className="aside-menu-link">
+            {link.text}
+          </a>
+        ))}
+        <a
+          href="https://google.com"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="aside-menu-link resume-link"
+        >
+          Currículo
+        </a>
+        <ul className="aside-menu-social-list-container">
+          {icons.map((icon) => (
+            <li key={icon.url} className="aside-menu-social-list-item">
+              <SVGIcon iconProps={{ ...icon }} />
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </aside>
+  );
+}
