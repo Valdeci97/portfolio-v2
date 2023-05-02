a0eebbeeaf37b77892ee179bb36eba17054e0121
valdeci <moura97.valdeci@gmail.com>
2023-05-02 17:08:29 -0300

    refactor(app style): redefine base color and apply changes at app

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,3 +1,21 @@
+import Header from './components/Header';
+import SocialMedia from './components/SocialMedia';
+import Home from './pages/Home';
+import { useWindowWidth } from './hooks/useWindowWidth';
+import './styles/app.css';
+
 export default function App() {
-  return <>Hello World.</>;
+  const { isMobileDevice } = useWindowWidth();
+
+  return (
+    <>
+      <Header isMobileDevice={isMobileDevice} />
+      {!isMobileDevice && <SocialMedia />}
+      <div className="pages-container">
+        <main className="main-container">
+          <Home />
+        </main>
+      </div>
+    </>
+  );
 }
--- app/src/tests/app.test.tsx
+++ app/src/tests/app.test.tsx
@@ -2,7 +2,7 @@ import App from '../App';
 import { render, screen } from './utils';
 
 test('Should render Hello world', () => {
-  render(<App />)
+  render(<App />);
   const main = screen.getByText(/hello world/i);
   expect(main).toBeInTheDocument();
 });
