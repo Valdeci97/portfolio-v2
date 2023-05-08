e70ec53a933a0b4b78e02ea6ce083442fa9c2eda
valdeci <moura97.valdeci@gmail.com>
2023-05-08 16:28:35 -0300

    feat(cocmponents): use useScroll hook to help with header component responsive style

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -1,12 +1,15 @@
 import Navbar from './Navbar';
 import MobileNavbar from './MobileNavbar';
+import useScroll from '../hooks/useScroll';
 import '../styles/header.css';
 
 type HeaderProps = { isMobileDevice: boolean };
 
 export default function Header({ isMobileDevice }: HeaderProps) {
+  const { hideHeader } = useScroll();
+
   return (
-    <header className="header-container">
+    <header className={`header-container ${hideHeader}`}>
       {!isMobileDevice && <Navbar />}
       {isMobileDevice && <MobileNavbar />}
     </header>
