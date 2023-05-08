193ab75e8ba755e98dd46b434f4b958a22685167
valdeci <moura97.valdeci@gmail.com>
2023-05-08 16:29:57 -0300

    style(home): create responsive style

--- app/src/pages/Home.tsx
+++ app/src/pages/Home.tsx
@@ -1,14 +1,12 @@
 import '../styles/home.css';
 
-// style={{ height: '30%' }}
-
 export default function Home() {
   return (
-    <section className="home-container">
+    <section id="home" className="home-container">
       <span className="home-introduction">Olá! Eu sou o Valdeci de Moura.</span>
 
-      <h2 className="home-host-name">Desenvolvedor web full stack.</h2>
-      <span className="home-dev-stack">
+      <h2 className="home-dev">Desenvolvedor web full stack.</h2>
+      <span className="home-dev-summary">
         Sou um cara curioso que gosta de desafios e resolver problemas, estou
         focado em aprimorar meus conhecimentos através de projetos próprios,
         lendo livros de referência, documentações de tecnologias que me chamam
